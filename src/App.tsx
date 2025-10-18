459484e7c565569ecc4224966f6595749dd4dda0
xiaoshihou <xiaoshihou@tutamail.com>
2025-10-18 09:27:32 +0100

    tink

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,3 @@
-// import './App.css'
-// import { Map } from '@vis.gl/react-maplibre'
-
-// function App() {
-//   return <Map
-//     initialViewState={{
-//       longitude: -100,
-//       latitude: 40,
-//       zoom: 3.5
-//     }}
-//     style={{width: 600, height: 400}}
-//     mapStyle="https://demotiles.maplibre.org/style.json"
-//   />;
-// }
-//
-// export default App
 import React from 'react';
 import Map from './Map';
 
